2c92a1d368d97f881902c5d58b4078c677a2683a
codinwithAbhi <abhisheklokhande676@gmail.com>
2023-09-10 22:27:34 +0530

    redux

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, { useState,useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Products } from './features/products/Products'
 import { useSelector, useDispatch } from 'react-redux'
 import { Cart } from './features/cart/Cart'
 import { fetchAsync } from './features/cart/cartSlice'
 import './App.css'
 
-function App () {
+function App() {
   const [showCart, setShowCart] = useState(false)
   const items = useSelector(state => state.cart.items)
   const dispatch = useDispatch()
@@ -14,9 +14,11 @@ function App () {
   }, [])
   return (
     <div className='App'>
-      <button onClick={() => setShowCart(!showCart)}>
-        Show Cart[{items.length}]
-      </button>
+     <div className="cart-icon" onClick={() => setShowCart(!showCart)}>
+        <i className="fas fa-shopping-cart" style={{ fontSize: "80px" }}></i>
+        <sup className="cart-count">{items.length}</sup>
+      </div>
+
       {showCart ? <Cart /> : <Products />}
     </div>
   )
